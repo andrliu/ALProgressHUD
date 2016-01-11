45fa862e79dc9139623ec2d62b330eab80f52c5c
andrliu <dieseliu321@gmail.com>
2016-01-11 13:50:59 -0800

    Initial Commit

---
+++ ALProgressHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ALProgressHUD
+//
+//  Created by Andrew Liu on 1/11/16.
+//  Copyright © 2016 Andrew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
