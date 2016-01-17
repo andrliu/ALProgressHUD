1406e78e17a04157ec6fc8bd18883e749d8f337f
andrliu <dieseliu321@gmail.com>
2016-01-16 18:07:08 -0800

    first commit

---
+++ ALProgressHUD/ALProgressHUD.h
@@ -0,0 +1,13 @@
+//
+//  ALProgressHUD.h
+//  ALProgressHUD
+//
+//  Created by Andrew Liu on 1/11/16.
+//  Copyright © 2016 Andrew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALProgressHUD : UIView
+
+@end
---
+++ ALProgressHUD/ALProgressHUD.m
@@ -0,0 +1,21 @@
+//
+//  ALProgressHUD.m
+//  ALProgressHUD
+//
+//  Created by Andrew Liu on 1/11/16.
+//  Copyright © 2016 Andrew Liu. All rights reserved.
+//
+
+#import "ALProgressHUD.h"
+
+@implementation ALProgressHUD
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
